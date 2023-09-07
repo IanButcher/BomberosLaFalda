1a4ec8cda68a315ac1c49f507a7d0b975336d995
IanButcher <115805201+IanButcher@users.noreply.github.com>
2023-09-07 09:23:28 -0300

    Angular stuff

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { LogInComponent } from './log-in/log-in.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    LogInComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
