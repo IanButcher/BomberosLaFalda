7590285e216a90cc12ebb6b4949d347823461227
IgnacioGabrielRodriguez <115805498+IgnacioGabrielRodriguez@users.noreply.github.com>
2023-09-28 09:42:36 -0300

    Cosas Para que Funcione Bien

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { LoginComponent } from './pages/login/login.component';
+import { FormsModule } from '@angular/forms';
+
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { NavComponent } from './shared/nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    FooterComponent,
+    HeaderComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
