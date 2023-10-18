853f11ddfbc63d1aaf8f27cd95aaf4993c7e05a0
IgnacioGabrielRodriguez <igrodriguez@escuelasproa.edu.ar>
2023-10-18 09:39:02 -0300

    Prueba de Ruteo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { SiniestrosComponent } from './pages/siniestros/siniestros.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'siniestro', component: SiniestrosComponent},
+  {path:  '', redirectTo: 'home' , pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
